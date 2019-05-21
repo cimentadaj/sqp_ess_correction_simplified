c5129325d407625a105987390400ae93cc4f48f8
cimentadaj <cimentadaj@gmail.com>
2019-05-21 18:03:53 +0200

    Cleans up after non used files

--- shiny/sqp_correct/global.R
+++ shiny/sqp_correct/global.R
@@ -45,55 +45,11 @@ all_ids <- c("cntry", "idno")
 language_iso3 <- 
   read_csv("language_country_table.csv")
 
-# To bring the function to extract sddf data.
-source("mk_svyConfig.R")
-
-# Country-specific data on spficif sampling strata, which variables
-# haave the weights, etcc...
-read_csv2("svydesign_info_ESS6.csv") %>%
-  filter(domains %% 1 == 0) %>%
-  split(., .$country) ->
-  svyinfo
-
 # ess_df <-
 #   setNames(
 #     map(all_countries, ~ recode_missings(import_country(.x, rounds = 6))),
 #     all_countries
 #   )
-
-
-# Same as sqp_cmv but good for programming with. cmv_vars accepts a vector rather than ...
-sqp_cmv_str <- function(x, sqp_data, cmv_vars, standardized = TRUE, original_data, 
-                        cmv = NULL) {
-  if (!(is.data.frame(x) | is.matrix(x))) {
-    stop("`x` must be a correlation data frame or matrix")
-  }
-  if (length(cmv_vars) < 2) {
-    stop("You need to supply at least two variables to calculate the Common Method Variance", 
-         call. = FALSE)
-  }
-  sqp_data <- sqpr:::sqp_reconstruct(sqp_data, c("reliability", "validity"))
-  x <- sqpr:::matrix2tibble(x)
-  sqpr:::columns_present(x, sqp_data, cmv_vars)
-  selected_rows <- sqp_data[[1]] %in% cmv_vars
-  if (is.null(cmv)) 
-    cmv <- sqpr:::estimate_cmv(sqp_data[selected_rows, ])
-  if (!standardized && missing(original_data)) {
-    stop("Argument `standardized` was set to `FALSE` but the `original_data` argument was not supplied")
-  }
-  else if (!standardized && !missing(original_data)) {
-    stopifnot(is.data.frame(original_data))
-    if (!all(cmv_vars %in% names(original_data))) {
-      stop("Variables ",
-           paste(cmv_vars[!cmv_vars %in% names(original_data)], collapse = ", "),
-           " are not preset in `original_data`")
-    }
-    cmv <- prod(cmv, vapply(original_data[cmv_vars], stats::sd, 
-                            FUN.VALUE = numeric(1)))
-  }
-  corrected_corr <- tibble::as_tibble(sqpr:::replace_matrix_cmv(x, cmv, cmv_vars))
-  corrected_corr
-}
 
 sqp_cmv_cor_str <- function (x, sqp_data, cmv_vars, cmv = NULL) {
 
